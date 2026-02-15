f2ad307edd42079e0655ea078b900919e60c4763
bbrodriges <bender.rodriges@gmail.com>
2026-02-15 16:41:16 +0300

    wip

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "A simple feature-rich dynamic tiling window manager";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+
+    sxwm-src = {
+      url = "https://github.com/uint23/sxwm/archive/v1.7.tar.gz";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+
+    in {
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+
+        pname = "sxwm";
+        version = "1.7";
+
+        src = sxwm-src;
+
+        nativeBuildInputs = with pkgs; [
+          xorg.libX11
+          xorg.libXinerama
+          libgcc
+          gnumake
+        ];
+
+        buildPhase = ''
+          make clean sxwm
+        '';
+
+        installPhase = ''
+          mkdir -p $out/bin
+          cp sxwm $out/bin/
+        '';
+
+      };
+    };
+}
